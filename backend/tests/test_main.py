0bdb5914c4a7feed9ca7a5ba35bf28ce0a30b85a
DrVaishnaviKR <vaishnavirajeshshyni@gmail.com>
2026-02-10 12:21:54 +0530

    6th commit

---
+++ backend/tests/test_main.py
@@ -0,0 +1,31 @@
+import pytest
+from fastapi.testclint import TestClint
+from pathlib import Path
+import os
+import pickle
+import numpy as np
+
+#Import your app (adjust if you rename it)
+from backend.main import app
+
+client = TestClient(app)
+
+def test_health_check():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "API running"}
+
+def test_predict_valid_input():
+    payload = {
+        "area": 1500.0,
+        "bedrooms": 3}
+    response = client.post("/predict", json=payload)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert "Predicted_price" in data 
+    assert isinstance(data["Predicted_price"], (int, float))
+    assert data["Predicted_price"] > 0  #assuming house price should be positive
+
+    
